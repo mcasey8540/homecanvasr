6fd577753d431388a22b2cb1f9f37f69b696f63c
Michael Casey <mcasey8540@gmail.com>
2013-04-09 20:45:48 -0400

    Create Preappoval model. Add associations with Client Model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130406165737) do
+ActiveRecord::Schema.define(:version => 20130406164749) do
 
   create_table "cities", :force => true do |t|
     t.integer  "county_id"
@@ -28,7 +28,6 @@ ActiveRecord::Schema.define(:version => 20130406165737) do
     t.string   "move_timeframe"
     t.datetime "created_at",     :null => false
     t.datetime "updated_at",     :null => false
-    t.integer  "preapproval_id"
   end
 
   create_table "counties", :force => true do |t|
@@ -58,6 +57,7 @@ ActiveRecord::Schema.define(:version => 20130406165737) do
 
   create_table "preapprovals", :force => true do |t|
     t.string   "file_name"
+    t.integer  "client_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
